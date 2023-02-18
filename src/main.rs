f22ab548f3e7dc335f80e36dd978a01e6ab6c6d5
robo-monk <rrobomonk@gmail.com>
2023-02-18 18:18:47 +0100

    raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 extern crate termion;
-use termion::{color, style};
+use termion::{color, style, cursor};
+use termion::raw::IntoRawMode;
 
-use std::io;
+use std::io::{Write, stdout};
 
 
 fn main() {
-    println!("{}Red", color::Fg(color::Red));
-    println!("{}{} yo !", termion::clear::All, termion::cursor::Goto(1, 1));
+    let mut stdout = stdout().into_raw_mode().unwrap();
+
+    write!(stdout, "{}{}Hey there!", color::Fg(color::Red), cursor::Goto(1,1)).unwrap();
+
+    //println!("{}Red", color::Fg(color::Red));
+    //println!("{}{} yo !", termion::clear::All, termion::cursor::Goto(1, 1));
 }
 
 
